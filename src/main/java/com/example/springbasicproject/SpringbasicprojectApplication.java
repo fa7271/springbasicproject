6fba3ba220818a7d8d7063e6b0a8988718ff0ce3
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-07-22 01:42:20 +0900

    Initial commit

---
+++ src/main/java/com/example/springbasicproject/SpringbasicprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbasicproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbasicprojectApplication.class, args);
+	}
+
+}
