c6594c022acd5739d866ce0d7f5ae99463c82a51
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-07-25 02:09:44 +0900

    h2 설정

--- src/main/java/com/example/springbasicproject/domain/posts/PostsRepository.java
+++ src/main/java/com/example/springbasicproject/domain/posts/PostsRepository.java
@@ -2,5 +2,6 @@ package com.example.springbasicproject.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostsRepository extends JpaRepository<Posts,Long> { // JpaRepository<Entity클래스, PK타입> 을 상속하면 기본적인 CRUD 메소드가 자동으로 생성된다.
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+    // JpaRepository<Entity클래스, PK타입> 을 상속하면 기본적인 CRUD 메소드가 자동으로 생성된다.
 }
