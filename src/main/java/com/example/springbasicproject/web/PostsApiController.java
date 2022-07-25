02d19653a537b9a71532dac95e6f4e5fbd08f9fd
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-07-25 23:58:44 +0900

    등록,수정,조회 만들기

---
+++ src/main/java/com/example/springbasicproject/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.example.springbasicproject.web;
+
+import com.example.springbasicproject.service.posts.PostsService;
+import com.example.springbasicproject.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+}
