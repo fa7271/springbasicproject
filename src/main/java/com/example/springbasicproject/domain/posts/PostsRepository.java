c57a9533f54d290fd81c293926e65fb5b3c981c3
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-07-28 02:24:56 +0900

    index 설정, 게시물 등록, footer/header 분리 js는 따로 관리

--- src/main/java/com/example/springbasicproject/domain/posts/PostsRepository.java
+++ src/main/java/com/example/springbasicproject/domain/posts/PostsRepository.java
@@ -1,7 +1,15 @@
 package com.example.springbasicproject.domain.posts;
 
+import org.apache.catalina.LifecycleState;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface PostsRepository extends JpaRepository<Posts,Long> {
+import java.util.List;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
     // JpaRepository<Entity클래스, PK타입> 을 상속하면 기본적인 CRUD 메소드가 자동으로 생성된다.
+
+    @Query("SELECT a FROM Posts a ORDER BY a.id DESC ")
+    List<Posts> findAllDesc();
 }
