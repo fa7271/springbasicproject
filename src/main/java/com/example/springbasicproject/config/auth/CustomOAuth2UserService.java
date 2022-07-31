040af95ca71d8cb655fdd76047a09e7863e8ac2f
fa7271 <90543205+fa7271@users.noreply.github.com>
2022-07-31 22:56:42 +0900

    loginuser 어노테이션 생성

--- src/main/java/com/example/springbasicproject/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/example/springbasicproject/config/auth/CustomOAuth2UserService.java
@@ -25,8 +25,6 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        System.out.println("옴?");
-        System.out.println("userRequest = " + userRequest);
         OAuth2UserService delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
